f9c887ca5712ec4ba6dccf228ac336f64db3afe6
Leo Montgomery <leodmontgomery@gmail.com>
2026-04-09 16:44:53 -0600

    Moved my date enum class to a standalone file so I can use it in other files

---
+++ LeoStuff/MonthEnum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Date(Enum): # Enumeration class to assign the month numbers to month strings. Easier to maintain human readability
+    Jan = 1
+    Feb = 2
+    Mar = 3
+    Apr = 4
+    May = 5
+    Jun = 6
+    Jul = 7
+    Aug = 8
+    Sep = 9
+    Oct = 10
+    Nov = 11
+    Dec = 12
